7b2d9dfe0f748d7734282a0fc809e57903189f86
unknown <amusriv@gmail.com>
2015-11-29 04:29:09 +0530

    Added sql insert query post sign-up.

--- form-1/php/basicsignup.php
+++ form-1/php/basicsignup.php
@@ -5,8 +5,10 @@ session_start();
 $firstName = $_POST['form-first-name'];
 $lastName = $_POST['form-last-name'];
 $mailId = $_POST['form-email'];
-$password = $_POST['form-last-name'];
+$password = $_POST['form-password'];
 $user_type = $_POST['optradio'];
+
+//var_dump($_POST);
 
 $_SESSION['form-first-name'] = $firstName;
 $_SESSION['form-last-name'] = $lastName;
@@ -14,7 +16,9 @@ $_SESSION['form-email'] = $mailId;
 $_SESSION['form-password'] = $password;
 $_SESSION['user-type'] = $user_type;
 
+//var_dump($_SESSION);
 $redir_location = 'Location: ../detailreg-user.html';
+//$redir_location = 'Location: ../dbconnect.php';
 
 header($redir_location);
 die();
--- form-1/php/finishregistration.php
+++ form-1/php/finishregistration.php
@@ -1,12 +1,30 @@
 <?php
-
 session_start();
+include ("../dbconnect.php");
+var_dump($_SESSION);
+$firstName = $_SESSION["form-first-name"];
+$lastName = $_SESSION["form-last-name"];
+$mailId = $_SESSION["form-email"];
+$password = $_SESSION["form-password"];
+$passwordToStore = md5($password);
+$mobile_number = $_POST['form-mobile-number'];
+$alternative_mobile_number = $_POST['form-alternate-mobile'];
+$dob = $_POST['form-dob'];
+$gender = $_POST['form-gender'];
+$qualification = $_POST['form-qual'];
+$institution = $_POST['form-inst'];
+$occupation = $_POST['form-occ'];
+$address = $_POST['form-add'];
+$state = $_POST['form-state'];
+$pic = $_POST['form-pic'];
+$doc = $_POST['form-doc'];
+$district = $_POST['form-dist'];
+$location = $_POST['form-geo'];
+$userId = explode("@", $mailId)[0];
+$curDate = date("D M d, Y G:i");
 
-$firstName = $_SESSION['form-first-name'];
-$lastName = $_SESSION['form-last-name'];
-$mailId = $_SESSION['form-email'];
-$password = $_SESSION['form-password'];
-
+$sqlQuery = "INSERT INTO `m_user`(`user_id`,`first_name`,`last_name`,`email_id`,`mobile_number`,`alternative_mobile_number`,`date_of_birth`,`gender`,`qualification`,`institution`,`occupation`,`state`,`district`,`location`,`address`,`document_path`,`picture_path`,create_time`,`update_time`,`cud`,`verified`,`m_id`,`verifier_mid`,`pwd`) values ($userId,$firstName,$lastName,$mailId,$mobile_number,$alternative_mobile_number,$dob,$gender,$qualification,$institution,$occupation,$state,$district,$location,$address,$doc,$pic,$curDate,$curDate,'C',0,0,'1',$passwordToStore)";
+mysqli_query($conn,$sqlQuery);
 
 ?>
 
