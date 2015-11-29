b6db800058b02bd08d3736ddb88a9c3e9b72c1b0
ajbose <ajbose@paypal.com>
2015-11-29 11:08:05 +0530

    moderator-actions updated

--- form-1/php/moderator_action.php
+++ form-1/php/moderator_action.php
@@ -1,3 +1,20 @@
 <?php
 
+include("dbconnect.php");
+
+$decision = $_GET['action'];
+$userId = $_GET['userId'];
+
+if ($decision == "approve")
+{
+	$query = "UPDATE m_user SET verified=1 WHERE user_id=".$userId;
+	mysqli_query($conn, $query);
+}
+
+elseif ($decision == "decline") {
+	$query = "UPDATE m_user SET verified=-1 WHERE user_id=".$userId;
+	mysqli_query($conn, $query);
+}
+
+header("Location: moderator-approve.php");
 ?>
