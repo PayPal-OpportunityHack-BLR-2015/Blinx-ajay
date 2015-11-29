27fe57332b8bd8290c213bd42d5bce21bd4cc7f4
KaviyaRawat <kaviya.rawat@gmail.com>
2015-11-29 11:20:14 +0530

    Moderator approves but no decline.

--- form-1/moderator-approve.php
+++ form-1/moderator-approve.php
@@ -128,7 +128,7 @@
                                                 echo '<td><a href="#" data-toggle="modal" data-target="#myModal" role="button">' . $row_users['first_name'] .'</a></td>';
                                                 echo '<td><a href="' . $row_users['document_path'] . '">Document</a></td>';
                                                 echo '<td>' . $row_users['user_type'] . '</td>';
-                                                echo '<td><p><a href="#" class="btn btn-primary" role="button">Approve</a> <a href="#" class="btn btn-default"  data-toggle="modal" data-target="#reasonModal" role="button">Decline</a></p></td>';
+                                                echo '<td><p><a href="php/moderator_acion.php?action=approve&userId=' . $row_users['user_id'] . '"' . '" class="btn btn-primary" role="button">Approve</a> <a href="php/moderator_acion.php?action=reject&userId=' . $row_users['user_id'] . '"' . '" class="btn btn-default"  data-toggle="modal" data-target="#reasonModal" role="button">Decline</a></p></td>';
                                                 echo '</tr>';
                                             }
                                             ?>
